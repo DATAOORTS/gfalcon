9d5147b42a3f9a3846b53ba91637017dab11891b
DATAOORTS <153160168+DATAOORTS@users.noreply.github.com>
2024-08-17 18:01:39 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,65 +1,61 @@
-# c
+# c 1
 sudo apt install -y build-essential
+sudo apt install unzip zip jq -y
 sudo apt install -y gcc
 sudo apt-get install -y apt-transport-https
 sudo apt-get install -y gpg
-# r
+# r 2
 sudo apt install r-base r-base-dev -y
-# c#
+# c# 3
 sudo apt-get install -y dotnet-sdk-8.0
 sudo apt-get install -y aspnetcore-runtime-8.0
 sudo apt-get install -y dotnet-runtime-8.0
 sudo apt install zlib1g ca-certificates libc6 libstdc++6 libgcc-s1 libgssapi-krb5-2 libicu70 liblttng-ust1 libssl3 libunwind8 -y
-# go
+# go 4
 sudo apt install -y golang-go
-# c++
+# c++ 5
 sudo apt install -y g++
-# php
+# php 6
 sudo apt install php libapache2-mod-php -y
 sudo apt install php-cli php-cgi php-mysql php-pgsql -y
 sudo apt install -y software-properties-common
-# java
+# java 7
 sudo apt install -y openjdk-17-jdk
-# ruby
+# ruby 8
 sudo apt install -y ruby-full
-# python
-sudo add-apt-repository ppa:deadsnakes/ppa
+# python 9
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install -y python3.11
-# pip
+# pip 9.1
+sudo apt install -y python3-pip
 sudo apt install -y python3.11-venv
-# julia
-sudo add-apt-repository ppa:julia/julia
-sudo apt install -y julia
-# executor
-wget https://raw.githubusercontent.com/DATAOORTS/gfalcon/blob/main/gfalcon.py
-# jifer
+# executor 9.2
+wget https://raw.githubusercontent.com/DATAOORTS/gfalcon/main/gfalcon.py
+# jifer 9.3
 wget https://github.com/DATAOORTS/jifer/raw/main/jifer -O jifer && chmod +x jifer && wget https://raw.githubusercontent.com/DATAOORTS/jifer/main/.bashrc -O temp_bashrc && cat temp_bashrc >> ~/.bashrc && rm temp_bashrc && source ~/.bashrc && echo "JiFer Installed Successfully!"
-# codon
-/bin/bash -c "$(curl -fsSL https://exaloop.io/install.sh)"
-# rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup toolchain install stable
-# dart
-wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/dart.gpg
-echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | sudo tee /etc/apt/sources.list.d/dart_stable.list
-sudo apt-get install dart
-# bash
+# bash 10
 sudo apt-get -y install bash
-# cobol
+# cobol 11
 sudo apt install -y gnucobol4
-# scala
+# scala 12
 sudo apt-get install -y scala
-# elixir
+# elixir 13
 sudo apt install elixir erlang-dev erlang-xmerl -y
-# kotlin
-curl -s https://get.sdkman.io | bash
-sdk install kotlin
-# fortran
+# fortran 14
 sudo apt-get install -y gfortran
-# node.js
+# node.js 15
 sudo apt install -y nodejs
 sudo apt install -y npm
-# javascript
+# javascript 16
 echo "Installed With Nodejs"
-# typescript
+# typescript 17
 npm install -g typescript
+# mojo 18
+curl -s https://get.modular.com | sh -s -- 40218620-c1e9-4d17-8983-e5c71bf7a2e9
+modular install mojo
+BASHRC=$( [ -f "$HOME/.bash_profile" ] && echo "$HOME/.bash_profile" || echo "$HOME/.bashrc" )
+echo 'export MODULAR_HOME="/home/rjvishwa/.modular"' >> "$BASHRC"
+echo 'export PATH="/home/rjvishwa/.modular/pkg/packages.modular.com_mojo/bin:$PATH"' >> "$BASHRC"
+source "$BASHRC"
+# codon 19
+echo "y" | /bin/bash -c "$(curl -fsSL https://exaloop.io/install.sh)"
